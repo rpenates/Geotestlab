65073e00722bdefdd78b7df3cd5eeb5ad1240591
Rafael Peñates <andres.penates@gmail.com>
2020-05-20 23:05:49 -0500

    Initial Commit

---
+++ GeoTestlab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeoTestlab
+//
+//  Created by Rafael Peñates on 20/05/20.
+//  Copyright © 2020 Rafael Peñates. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
